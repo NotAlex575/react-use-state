51caa37c0bd5407a67ba97b3a95f2dc064e9efa0
NotAlex575 <dapontealessandro89gmail.com>
2025-08-28 16:39:37 +0200

    inizializzato header con div titolo

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () =>{
+    return(
+        <header>
+            <div className="titolo">
+                Titolo
+            </div>
+        </header>
+    )
+}
+
+export default Header;
